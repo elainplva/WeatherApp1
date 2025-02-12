3e2233f4fa716de4f889a6431ac80f997e6fd1d8
Elain Polakova <S00250500@atu.ie>
2025-02-12 11:48:59 +0000

    getting a json package

--- WeatherApp/Services/ApiService.cs
+++ WeatherApp/Services/ApiService.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherApp.Models;
 
 namespace WeatherApp.Services
 {
     public class ApiService
     {
-        public async void GetWeather(double latitude, double longitude)
+        public async Task<Root> GetWeather(double latitude, double longitude)
         {
             var httpClient = new HttpClient();
-           var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&appid=faeb14dd3f0d880980cf2137b29720e6", latitude,longitude));
+            var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&appid=faeb14dd3f0d880980cf2137b29720e6", latitude,longitude));
+            return JsonConvert.DeserializeObject<Root>(response);
         }
     }
 }
