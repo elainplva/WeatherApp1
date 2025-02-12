ff4e7cac90927f5d9dc120fd1459f7414cdc05a6
Elain Polakova <S00250500@atu.ie>
2025-02-12 11:41:37 +0000

    editing the api url call

---
+++ WeatherApp/Services/ApiService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Services
+{
+    public class ApiService
+    {
+        public async void GetWeather(double latitude, double longitude)
+        {
+            var httpClient = new HttpClient();
+           var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&appid=faeb14dd3f0d880980cf2137b29720e6", latitude,longitude));
+        }
+    }
+}
